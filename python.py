56d802f566f1a570d7c69391f2a17973234db829
root <root@ip-172-31-8-140.eu-north-1.compute.internal>
2026-02-14 18:22:02 +0000

    updated the gaming code in python

--- python.py
+++ python.py
@@ -17,7 +17,29 @@ for number in numbers:
 print('The product is:', product)
 
 #Third code
+def number_guessing_game():
+    secret_number = random.randint(1, 100) # Generate a random number between 1 and 100
+    print("Guess the number between 1 and 100.")
+    
+    while True:
+        try:
+            guess = int(input("Enter your guess: "))
+            
+            if guess == secret_number:
+                print("Congratulations! You guessed the number!")
+                break
+            elif guess < secret_number:
+                print("Too low! Try again.")
+            else:
+                print("Too high! Try again.")
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
+
+if __name__ == "__main__":
+    number_guessing_game()
+
+
+#forth code
 fruits = ['Banana', 'Apple', 'Lime']
 loud_fruits = [fruit.upper() for fruit in fruits]
 print(loud_fruits)
-
