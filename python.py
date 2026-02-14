0fcd09f81981e0e0d3db30f2d84f4d73cc86d7fb
root <root@ip-172-31-8-140.eu-north-1.compute.internal>
2026-02-14 17:58:51 +0000

    latest updated code

--- python.py
+++ python.py
@@ -16,7 +16,19 @@ for number in numbers:
     product = product * number
 print('The product is:', product)
 
-#Third code
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
+
+
+
+
+
+
+
+
+#Fourth code
 fruits = ['Banana', 'Apple', 'Lime']
 loud_fruits = [fruit.upper() for fruit in fruits]
 print(loud_fruits)
